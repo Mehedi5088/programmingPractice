b2a26d30143bfbd25416d851b7428a03e3c78aa0
Mehedi5088 <mhasan.egov@gmail.com>
2026-08-05 22:27:15 +0600

    This is programming practice first commit

---
+++ nestedLoop.js
@@ -0,0 +1,26 @@
+
+// i=0 
+// *
+// **
+// ***
+// i= 4 j = 0,1,2,3,
+// i= 5 j = 0,1,2,3,4
+// i= 6 j = 0,1,2,3,4,5
+
+class StarPring{
+    star(n){
+        let star = "";
+        for (let i = 0; i < n; i++) {
+           for (let j = 0; j < i; j++) {
+                star = star + "*";
+           }
+         console.log(star);
+        }
+    }
+}
+
+const result = new StarPring();
+result.star(7);
+
+// find odd and even number 15
+// find the average value from the given array
---
+++ problemSolving2.js
@@ -0,0 +1,28 @@
+// array = [10,55,2,4,19,68,0,14,24]
+// Find max value from the given Array
+
+// dhori max = 10
+// max value ki array er first value thke boro : max > arra[0]
+// max will not change ,
+// else : max vlaue will be the array value.
+// max value ki array er second value thke boro naki soto max > arra[1]
+// max value ki array er second value thke boro naki soto max > arra[2]
+// loop increment i++
+
+class FindMaxValue{
+    maxValue(){
+        let price = [10,55,2,4,19,68,0,14,24];
+        let max = "0";
+        for (let i = 0; i < price.length; i++) {
+           if (max < price[i]) {
+            max = price[i];
+           }
+        }
+        return max;
+
+    }
+}
+
+const result = new FindMaxValue();
+console.log(result.maxValue());
+
