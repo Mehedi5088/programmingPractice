88227005ec1c869f0901c2bd2c93a468ff314415
Mehedi5088 <mhasan.egov@gmail.com>
2026-08-10 20:07:48 +0600

    ghdfg

--- nestedLoop.js
+++ nestedLoop.js
@@ -24,11 +24,6 @@ result.star(7);
 
 // find odd and even number 15
 // find the average value from the given array
-<<<<<<< HEAD
 // it is more confusing to find the average value from the given array.
 
 // again data is gone
-=======
-
-// it is conflict with the previous code snippet, so I will provide a new implementation for finding odd and even numbers and calculating the average value from a given array.
->>>>>>> fbfd9561210a9ad52878f432bc062fb0ee3452d2
